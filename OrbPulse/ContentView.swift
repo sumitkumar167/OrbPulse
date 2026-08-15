f8f0928f9dd01a96dcf08530442ea20a357810a5
Sumit Kumar <sumitblock@gmail.com>
2026-08-14 21:36:25 -0400

    Initial Commit

---
+++ OrbPulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrbPulse
+//
+//  Created by Sumit Kumar on 14/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrbPulse/OrbPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbPulseApp.swift
+//  OrbPulse
+//
+//  Created by Sumit Kumar on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct OrbPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
